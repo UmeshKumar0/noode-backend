4f08da206c3752ed9d18bd55985800b163a09d03
umesh Kumar <uk289559@gmail.com>
2023-02-06 11:36:24 +0530

    Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 //Initialise express
-const  express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -11,18 +11,14 @@ const mongoose = require('mongoose');
 const Note = require('./models/Note');
 
 //Connect to MongoDB
-mongoose.connect('mongodb+srv://vamp_goku:Kiyanyadav%401@cluster0.kfzmhcg.mongodb.net/notesdb', {useNewUrlParser: true}).then(()=> { 
-    
-console.log('MongoDB connected...')
-
-
-app.use("/api", noteRouter);
+mongoose.connect('mongodb+srv://vamp_goku:Kiyanyadav%401@cluster0.kfzmhcg.mongodb.net/notesdb', { useNewUrlParser: true }).then(() => {
+        console.log('MongoDB connected...')
+        app.use("/api", noteRouter);
 }).catch(err => console.log(err));
 
 
 
 
-
-        //Listen to port 5000 (Start server)
-        const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server started on port '+ PORT));
+//Listen to port 5000 (Start server)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Server started on port ' + PORT));
